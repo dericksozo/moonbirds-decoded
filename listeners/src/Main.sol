4c1801e483ff4388bf5a273f6a465da034e4426b
Derick Ruiz <derick.r.ruiz@gmail.com>
2025-06-28 07:47:58 +0000

    Adding all the new files related to Moonbirds. Removing the Uniswap V3 stuff.

--- listeners/src/Main.sol
+++ listeners/src/Main.sol
@@ -8,10 +8,10 @@ contract Triggers is BaseTriggers {
     function triggers() external virtual override {
         Listener listener = new Listener();
         addTriggers(
-            ChainIdContract(1, 0x1F98431c8aD98523631AE4a59f267346ea31F984),
+            ChainIdContract(1, 0x23581767a106ae21c074b2276D25e5C3e136a68b),
             listener.allTriggers()
         );
     }
 }
 
-contract Listener is UniswapV3Factory$EmitAllEvents {}
+contract Listener is Moonbirds$EmitAllEvents {}
